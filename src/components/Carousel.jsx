b7168606d63423b517cd75caa87a2e9f4c96ae4f
onaolapomay <raphy.mayo@gmail.com>
2025-07-04 07:55:32 -0600

    your update message

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -18,9 +18,9 @@ if(!images || images.length === 0) return null;
 return (
     <div className='bg-gray-100 flex flex-col md:flex-row space-x-32 items-center justify-center'>
         <div className='flex justify-center items-center mt-5'>
-            <p className='text-center font-ultra-expanded text-2xl underline font-[nunito]'>EXPLORE THE COLLECTIONS</p>
+            <p className='font-ultra-expanded text-2xl underline font-[nunito]'>EXPLORE THE COLLECTIONS</p>
         </div>
-    <div className='relative w-full max-w-2xl overflow-hidden sm:justify-center  cursor-pointer sm:mt-2 md:mt-5 lg:mt-10'>
+    <div className='relative w-full max-w-2xl overflow-hidden  cursor-pointer mt-4'>
          <span className="text-black sm:text-xl md:text-2xl lg:3xl sm:text-center font-[winky_sans]">Revamp Softbit large shoulder bag</span>
         <div className='flex transition-transform duration-700 ease-in-out' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
             {images.map((src, idx) => (
