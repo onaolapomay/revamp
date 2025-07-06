038093681eab174a673c812c2b56e502846e4a29
onaolapomay <raphy.mayo@gmail.com>
2025-07-06 06:12:04 -0600

    your update message

--- src/components/TrendReport.jsx
+++ src/components/TrendReport.jsx
@@ -10,16 +10,16 @@ const TrendReport = () => {
 
                 <div className='mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
                     <div className='group relative'>
-                        <img src="./media/trend-report/cardimg.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-center group-hover:opacity-75 lg:aspect-auto lg:h-100' />
+                        <img src="./media/trend-report/cardimg.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-100' />
                     </div>
                     <div className='group relative'>
-                        <img src="./media/trend-report/cardimg2.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-center group-hover:opacity-75 lg:aspect-auto lg:h-100' />
+                        <img src="./media/trend-report/cardimg2.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-100' />
                     </div>
                     <div className='group relative'>
-                        <img src="./media/trend-report/cardimg3.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-center group-hover:opacity-75 lg:aspect-auto lg:h-100' />
+                        <img src="./media/trend-report/cardimg3.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-100' />
                     </div>
                     <div className='group relative'>
-                        <img src="./media/trend-report/cardimg4.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-center group-hover:opacity-75 lg:aspect-auto lg:h-100' />
+                        <img src="./media/trend-report/cardimg4.jpg" alt="Front of men's Basic Tee in black." className='aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-100' />
                     </div>
 
                 </div>
