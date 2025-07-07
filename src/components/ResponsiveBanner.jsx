53d4d970100f080e8cd332d0cebaba0d29d64ce7
onaolapomay <raphy.mayo@gmail.com>
2025-07-07 06:50:44 -0600

    your update message

--- src/components/ResponsiveBanner.jsx
+++ src/components/ResponsiveBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ResponsiveBanner = () => {
     return (
-        <div className="w-full h-[380px] lg:h-[500px] bg-no-repeat bg-cover bg-center mt-10 cursor-pointer
+        <div className="w-full h-svh lg:h-[500px] bg-no-repeat bg-cover bg-center mt-10 cursor-pointer
             bg-[url('/background-images/back3_sm.jpg')] 
             lg:bg-[url('/background-images/back3_lg.jpg')]">
         </div>
