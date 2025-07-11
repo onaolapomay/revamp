02e81298238cfba668096e8f47ddc19694d4ed8e
onaolapomay <raphy.mayo@gmail.com>
2025-07-11 04:40:06 -0600

    your update message

--- src/components/ShopLatestGrid.jsx
+++ src/components/ShopLatestGrid.jsx
@@ -4,7 +4,7 @@ import ProductCards from './ProductCards';
 
 const ShopLatestGrid = () => {
     return(
-        <div className='mt-6 px-8 sm:px-16 md:px-32'>
+        <div className='mt-6 px-4 sm:px-16 md:px-32'>
             <div className='grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {colorProducts.map(product => (
                     <ProductCards key={product.id} product={product} />
